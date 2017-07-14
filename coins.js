96aacbfd9d9201c9185afc2e9545e851ce84501b
Chandler <cwm795@gmail.com>
2017-07-13 22:38:14 -0500

    fixed many bugs and mostly finished this up. look at line 3.

--- coins.js
+++ coins.js
@@ -1,49 +1,36 @@
-/*
-  Input: 0.67
-  Output:
-  {
-    quarters: 2,
-    dimes: 1,
-    nickels: 1,
-    pennies: 2
-  }
+var input = document.getElementById("input");
 
-*/
-var input = document.getElementById("dollarInput");
-
-input.addEventListener("keyup", function(event) {
+input.addEventListener("keyup", function(e) {
     event.preventDefault();
     if (event.keyCode === 13) {
-        input = input.value = "";
-        coinCounter();
+        input = input.value
+
+        function coinCounter(input) {
+            // Initialize a JavaScript object to hold the coins
+            var coinPurse = {};
+
+            coinPurse.quarters = 0;
+            coinPurse.dimes = 0;
+            coinPurse.nickels = 0;
+            coinPurse.pennies = 0;
 
 
+            coinPurse.quarters = Math.floor(input / .25);
+            remainder = (input % .25);
+            coinPurse.dimes = Math.floor(remainder / .10);
+            remainder = (input % .10);
+            coinPurse.nickels = Math.floor(remainder / .05);
+            remainder = remainder % .05;
+            coinPurse.pennies = Math.floor(remainder / .01);
 
 
+            return coinPurse;
 
-    function coinCounter(input) {
-        // Initialize a JavaScript object to hold the coins
-        var coinPurse = {};
+        }
 
-        coinPurse.quarters = 0;
-        coinPurse.dimes = 0;
-        coinPurse.nickels = 0;
-        coinPurse.pennies = 0;
-
-
-        coinPurse.quarters = Math.floor(input / .25);
-        remainder = (input % .25);
-        coinPurse.dimes = Math.floor(remainder / .10);
-        remainder = (input % .10);
-        coinPurse.nickels = Math.floor(remainder / .05);
-        remainder = remainder % .05;
-        coinPurse.pennies = Math.floor(remainder / .01);
-
-
-        return coinPurse;
-
+        var coins = coinCounter(input)
+        console.log(coins);
     }
 
-    var coins = coinCounter(input)
-    }
+
 });
